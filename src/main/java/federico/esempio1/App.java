3a371d2b7764b6f950b0bca9b69373477b72b3ac
f.piscitelli1@campus.unimib.it <f.piscitelli1@campus.unimib.it>
2019-10-29 10:24:22 +0100

    Lab 3 - Esercizio 1 (Paragrafo 3)

---
+++ src/main/java/federico/esempio1/App.java
@@ -0,0 +1,14 @@
+package federico.esempio1;
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
